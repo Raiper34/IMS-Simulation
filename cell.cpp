5cfbc4ce5b86c8c99d869878779aa674839db463
Raiper34 <raipergm34@gmail.com>
2015-12-06 23:26:54 +0100

    comments in cell

--- cell.cpp
+++ cell.cpp
@@ -1,16 +1,27 @@
-//
-// Created by marek on 11/21/15.
-//
+/*
+ * IMS Project - Application of celular automata in biology
+ * Authors: Filip Gulan and Marek Marusic
+ * E-mails: xgulan00@stud.fit.vutbr.cz and xmarus05@stud.fit.vutbr.cz
+ */
 
 #include "cell.h"
 
+/*
+ * Constructor of class
+ */
 cell::cell()
 {
-    i = 0;
-    j = 0;
-    state = 0;
+    i = EMPTY;
+    j = EMPTY;
+    state = EMPTY;
 };
 
+/*
+ * Method, to set data
+ * @i horizontal position in matrix
+ * @j vertical position in matrix
+ * @state of cell
+ */
 void cell::setData(int i, int j, int state)
 {
     this->i = i;
@@ -18,6 +29,10 @@ void cell::setData(int i, int j, int state)
     this->state = state;
 }
 
+/*
+ * Method, to set state
+ * @state state to set
+ */
 void cell::setState(int state)
 {
     this->state = state;
--- cell.h
+++ cell.h
@@ -1,5 +1,13 @@
+/*
+ * IMS Project - Application of celular automata in biology
+ * Authors: Filip Gulan and Marek Marusic
+ * E-mails: xgulan00@stud.fit.vutbr.cz and xmarus05@stud.fit.vutbr.cz
+ */
+
 #ifndef IMS_PROJEKT_CELL_H
 #define IMS_PROJEKT_CELL_H
+
+#define EMPTY 0
 
 using namespace std;
 
